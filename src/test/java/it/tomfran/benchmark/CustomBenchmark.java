02b6f040e63c70b42b9b7196eb0e79b629496843
tomfran <18344761+tomfran@users.noreply.github.com>
2022-09-27 11:52:25 +0200

    Updated thesis and abstract

--- src/test/java/it/tomfran/benchmark/CustomBenchmark.java
+++ src/test/java/it/tomfran/benchmark/CustomBenchmark.java
@@ -1,42 +1,33 @@
 package it.tomfran.benchmark;
 
 import it.tomfran.thesis.graph.AnsGraph;
-import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 import it.unimi.dsi.webgraph.BVGraph;
 import it.unimi.dsi.webgraph.EFGraph;
 import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.LazyIntIterator;
-import it.unimi.dsi.webgraph.algo.GeometricCentralities;
-import it.unimi.dsi.webgraph.algo.ParallelBreadthFirstVisit;
-import it.unimi.dsi.webgraph.algo.StronglyConnectedComponents;
-import it.unimi.dsi.webgraph.algo.TopKGeometricCentrality;
-
 import java.io.IOException;
-
-import static it.unimi.dsi.webgraph.algo.TopKGeometricCentrality.Centrality.HARMONIC;
 
 public class CustomBenchmark {
 
     public static void main(String[] args) throws IOException {
 
-        int WARMUP = 1000;
+        int WARMUP = 2000;
         int REPEAT = 5000;
-        int samples = 1000;
+        int samples = 1500;
 
-//        CharSequence graph_name = "cnr-2000";
-        CharSequence graph_name = "uk-2007-05@1000000";
+//        CharSequence graph_name = "uk-2007-05@1000000";
 //        CharSequence graph_name = "indochina-2004";
 //        CharSequence graph_name = "enwiki-2013";
-//        CharSequence graph_name = "dblp-2011";
+        CharSequence graph_name = "dblp-2011";
         CharSequence basename = "data/" + graph_name + "/nat/";
-        ImmutableGraph graph = AnsGraph.load(basename + "clustered_ans/ans");
+//        ImmutableGraph graph = AnsGraph.load(basename + "clustered_ans/ans");
 //        ImmutableGraph graph = EFGraph.load(basename + "ef/" + graph_name);
-//        ImmutableGraph graph = BVGraph.load(basename + "bv/" + graph_name);
+        ImmutableGraph graph = BVGraph.load(basename + "bv/" + graph_name);
 
-        XoRoShiRo128PlusRandom r = new XoRoShiRo128PlusRandom(1000);
+        XoRoShiRo128PlusRandom r = new XoRoShiRo128PlusRandom();
         final int n = graph.numNodes();
-        int seed = 0;
+        int seed = 123;
         long z = -1;
         long cumulativeTime = 0;
         int totLinks = 0;
